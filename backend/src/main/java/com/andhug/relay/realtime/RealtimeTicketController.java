c76d9105e384999f247da4ed4f1d344364a36fe2
hughesandrewb <hughesandrewb@gmail.com>
2026-03-21 18:21:58 -0500

    refactor: add base path to RealtimeTicketController

--- backend/src/main/java/com/andhug/relay/realtime/RealtimeTicketController.java
+++ backend/src/main/java/com/andhug/relay/realtime/RealtimeTicketController.java
@@ -6,23 +6,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/realtime")
 @RequiredArgsConstructor
 public class RealtimeTicketController {
 
-        private final RealtimeTicketService realtimeTicketService;
+    private final RealtimeTicketService realtimeTicketService;
 
-        @GetMapping("/api/realtime/ticket")
-        public ResponseEntity<RealtimeTicketDto> getRealtimeTicket(
-                        @AuthenticationPrincipal Profile profile) {
+    @GetMapping("/ticket")
+    public ResponseEntity<RealtimeTicketDto> getRealtimeTicket(
+            @AuthenticationPrincipal Profile profile) {
 
-                String realtimeTicket = realtimeTicketService.getRealtimeTicket(profile);
+        String realtimeTicket = realtimeTicketService.getRealtimeTicket(profile);
 
-                return ResponseEntity.ok(
-                                RealtimeTicketDto.builder()
-                                                .code(realtimeTicket)
-                                                .build());
-        }
+        return ResponseEntity.ok(
+                RealtimeTicketDto.builder()
+                        .code(realtimeTicket)
+                        .build());
+    }
 }
